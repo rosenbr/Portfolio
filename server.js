49458c29ab8bfbe15fb8200eeeda3804f25889c9
Richard Rosenberg <rosenbr73@gmail.com>
2021-07-26 14:20:47 -0700

    Stubbed it up

---
+++ server.js
@@ -0,0 +1,21 @@
+// Run thru installs
+// Deploy on Heroku
+// anything else?
+
+// === | External Modules | ===
+
+// === | Internal Modules | ===
+
+// === | Instanced Modules | ===
+
+// === | Config | ===
+
+// === | Middleware | ===
+
+// === | Routes | ===
+
+// === | Controllers | ===
+
+// === | Listener | ===
+
+// === | Local Testing Listener | ===
