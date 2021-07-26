7578b85060dffa5384677008b3d08c815b81c3ec
Richard Rosenberg <rosenbr73@gmail.com>
2021-07-26 14:59:17 -0700

    Server Running

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // TODO
-// Run thru installs
 // Build functionality
 // Build Structure
 // CSS
@@ -7,19 +6,33 @@
 // anything else?
 
 // === | External Modules | ===
+const express = require("express");
+// const methodOverride = require("method-override");
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo");
 
 // === | Internal Modules | ===
+// const controllers = require("./controllers");
 
 // === | Instanced Modules | ===
+const app = express();
 
 // === | Config | ===
+app.set("view engine", "ejs");
 
 // === | Middleware | ===
 
 // === | Routes | ===
+// - | Home | -
+app.get("/", function(req, res){
+    res.render("../views/home");
+});
 
 // === | Controllers | ===
 
 // === | Listener | ===
 
 // === | Local Testing Listener | ===
+app.listen(3000, function(){
+    console.log("Running on PORT 3000");
+});
